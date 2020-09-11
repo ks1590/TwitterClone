a5a53b234a8a31bdb79a2f0ce7f5dc81f8f254f9
kensuke suzuki <35k.0101@gmail.com>
2020-09-11 19:52:46 +0900

    formパーシャル作成

--- app/controllers/twittters_controller.rb
+++ app/controllers/twittters_controller.rb
@@ -1,4 +1,5 @@
 class TwitttersController < ApplicationController
+  before_action :set_twittter, only: [:show, :edit, :update]
   def index
     @twittters = Twittter.all
   end
@@ -17,9 +18,28 @@ class TwitttersController < ApplicationController
     end
   end
 
+  def show
+  end
+  
+  def edit
+  end
+
+  def update
+    if @twittter.update(twittter_params)
+      redirect_to home_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+  
+  
   private
   def twittter_params
     params.require(:twittter).permit(:content)
+  end
+  
+  def set_twittter
+    @twittter = Twittter.find(params[:id])
   end
   
 end
